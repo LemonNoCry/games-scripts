76c32f62c28c2a5d25e5d732303d26a65981672c
LemonNoCry <ningmengbuku@outlook.com>
2025-10-23 15:46:07 +0800

    🔧 更新版本号至 1.0.1

--- idle-games/clean-web-ui.user.js
+++ idle-games/clean-web-ui.user.js
@@ -1,10 +1,9 @@
 // ==UserScript==
 // @name         移除、精简UI、
-// @match        *://*/*
 // @run-at       document-end
 // @namespace    LemonNoCry
 // @license      MIT
-// @version      1.0.0
+// @version      1.0.1
 // @description  移除游戏中的广告和多余UI元素，提升游戏体验。
 // @match        https://gltyx.github.io/*
 // @match        https://cosmic-collection.g8hh.com.cn/*
